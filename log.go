8e84f2023ae9d6c50859a94c88af418f367f70a4
Peter Verraedt <peter@verraedt.be>
2023-03-13 18:00:46 +0100

    Set logging configurable
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- log.go
+++ log.go
@@ -1,15 +1,22 @@
 package useros
 
 import (
-	"log"
+	"fmt"
 	"runtime"
 )
 
+// LogHandler is a configurable log handler
+var LogHandler func(error)
+
 func logit(err error) error {
+	if LogHandler == nil {
+		return err
+	}
+
 	_, file, line, _ := runtime.Caller(1)
 
 	if err != nil {
-		log.Printf("ERR %s %d: %s", file, line, err)
+		LogHandler(fmt.Errorf("ERR at %s %d: %w", file, line, err))
 	}
 
 	return err
